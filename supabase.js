635f8a51fb4c9c2dd26e892e3d27fa65683cc27b
SohyangJang <yabaezzae@gmail.com>
2026-07-17 14:51:35 +0900

    Update Supabase key for client initialization

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://ootgfnycumxevuilhmwl.supabase.co";
-const SUPABASE_KEY = "sb_publishable__sFQUs8WzrIi7JsYvZSfBQ_6hApSfx2";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vdGdmbnljdW14ZXZ1aWxobXdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyMzAyMDIsImV4cCI6MjA5OTgwNjIwMn0.bk5abiU_GzTYYCx1Vww5NOnRc85AxdyaTj9IAZyVE5w";
 
 // HTML에서 불러온 CDN(window.supabase)을 이용하여 클라이언트를 생성합니다.
 window.supabaseClient = window.supabase.createClient(
