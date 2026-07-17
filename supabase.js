40b377f6a8fcb901d95ee072429a5d8ae1951aa6
SohyangJang <yabaezzae@gmail.com>
2026-07-17 15:00:03 +0900

    Refactor Supabase client initialization

--- supabase.js
+++ supabase.js
@@ -1,8 +1,7 @@
-const SUPABASE_URL = "https://ootgfnycumxevuilhmwl.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vdGdmbnljdW14ZXZ1aWxobXdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyMzAyMDIsImV4cCI6MjA5OTgwNjIwMn0.bk5abiU_GzTYYCx1Vww5NOnRc85AxdyaTj9IAZyVE5w";
+window.SUPABASE_URL = "https://ootgfnycumxevuilhmwl.supabase.co";
+window.SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vdGdmbnljdW14ZXZ1aWxobXdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyMzAyMDIsImV4cCI6MjA5OTgwNjIwMn0.bk5abiU_GzTYYCx1Vww5NOnRc85AxdyaTj9IAZyVE5w";
 
-// HTML에서 불러온 CDN(window.supabase)을 이용하여 클라이언트를 생성합니다.
 window.supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
+    window.SUPABASE_URL,
+    window.SUPABASE_KEY
 );
