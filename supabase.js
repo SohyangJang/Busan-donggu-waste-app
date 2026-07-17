3fc542d3e2f657f42087d80db4c4355f97c5e0cb
SohyangJang <yabaezzae@gmail.com>
2026-07-17 14:16:12 +0900

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://ootgfnycumxevuilhmwl.supabase.co";
+const SUPABASE_KEY = "sb_publishable__sFQUs8WzrIi7JsYvZSfBQ_6hApSfx2";
+
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
