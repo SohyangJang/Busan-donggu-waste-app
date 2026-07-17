50f95ad7b1362da77a258354eef3242d12486f76
SohyangJang <yabaezzae@gmail.com>
2026-07-17 15:15:22 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const SUPABASE_URL = "https://ootgfnycumxevuilhmwl.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vdGdmbnljdW14ZXZ1aWxobXdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyMzAyMDIsImV4cCI6MjA5OTgwNjIwMn0.bk5abiU_GzTYYCx1Vww5NOnRc85AxdyaTj9IAZyVE5w";
 
 window.supabaseClient = window.supabase.createClient(
-    window.SUPABASE_URL,
-    window.SUPABASE_KEY
+    SUPABASE_URL,
+    SUPABASE_KEY
 );
 
 
