288b35008e4a750f25215f7b0966dcb9923324b3
SohyangJang <yabaezzae@gmail.com>
2026-07-17 14:22:28 +0900

    Refactor supabaseClient creation to use window object
    Updated supabaseClient initialization to use window.supabase.

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
 const SUPABASE_URL = "https://ootgfnycumxevuilhmwl.supabase.co";
 const SUPABASE_KEY = "sb_publishable__sFQUs8WzrIi7JsYvZSfBQ_6hApSfx2";
 
-
-const supabaseClient = supabase.createClient(
+// HTML에서 불러온 CDN(window.supabase)을 이용하여 클라이언트를 생성합니다.
+window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
