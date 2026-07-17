eaa849ab45be28f1a5a275a695d629e406aa285a
SohyangJang <yabaezzae@gmail.com>
2026-07-17 14:47:00 +0900

    Fix string formatting for SUPABASE_URL and SUPABASE_KEY

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = https://ootgfnycumxevuilhmwl.supabase.co;
-const SUPABASE_KEY = sb_publishable__sFQUs8WzrIi7JsYvZSfBQ_6hApSfx2;
+const SUPABASE_URL = "https://ootgfnycumxevuilhmwl.supabase.co";
+const SUPABASE_KEY = "sb_publishable__sFQUs8WzrIi7JsYvZSfBQ_6hApSfx2";
 
 // HTML에서 불러온 CDN(window.supabase)을 이용하여 클라이언트를 생성합니다.
 window.supabaseClient = window.supabase.createClient(
